cca7061a1402944783d603d6b5c64b4b7a1dd99d
Arx1971 <adnan.rahin13@gmail.com>
2020-01-06 23:03:18 -0800

    Configuration File Added

--- src/main/java/com/spring/framework/SpringHelloApp.java
+++ src/main/java/com/spring/framework/SpringHelloApp.java
@@ -1,5 +1,6 @@
 package com.spring.framework;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringHelloApp {
@@ -8,6 +9,11 @@ public class SpringHelloApp {
 
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        System.out.println(theCoach.getDailyWorkout());
+
+        context.close();
 
     }
 
