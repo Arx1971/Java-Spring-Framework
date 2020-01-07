e86312c9d4aafd8521abbc012982385c593a77be
Arx1971 <adnan.rahin13@gmail.com>
2020-01-07 15:57:11 -0800

    Constructor Injection

--- src/main/java/com/spring/framework/BaseballCoach.java
+++ src/main/java/com/spring/framework/BaseballCoach.java
@@ -1,14 +1,24 @@
 package com.spring.framework;
 
 public class BaseballCoach implements Coach {
+
+    // private field for dependency
+    private FortuneService fortuneService;
+
+
+    // Constructor for Dependency Injection
+    public BaseballCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         return "Spend 30 minutes on batting practice";
     }
 
     @Override
     public String getDailyFortune() {
-        return null;
+        return fortuneService.getFortune();
     }
 
 }
