af94bbb2aa217136db683d8e591dcb2edad08dd0
Arx1971 <adnan.rahin13@gmail.com>
2020-01-06 23:43:03 -0800

    Configuration File Added

---
+++ src/main/java/com/spring/framework/SoccerCoach.java
@@ -0,0 +1,9 @@
+package com.spring.framework;
+
+public class SoccerCoach implements Coach {
+
+    public String getDailyWorkout() {
+        return "They have working out for last few hours";
+    }
+
+}
