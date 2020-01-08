ff0e168ca291ebea6f72f9b70770535b23b1c1f5
Arx1971 <adnan.rahin13@gmail.com>
2020-01-07 22:38:38 -0800

    Setter Injectin

---
+++ src/main/java/com/spring/framework/SetterDemoApp.java
@@ -0,0 +1,28 @@
+package com.spring.framework;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+    public static void main(String args[]) {
+
+        // Load Spring Configuration File
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve bean from spring container
+
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+        // call methods on the bean
+
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+
+        // close the context
+
+        context.close();
+
+    }
+
+}
